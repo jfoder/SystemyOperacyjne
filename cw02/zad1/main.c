f0847f22d9e7cac8149218cbc13da8b5a80ca88a
jfoder <jaroslaw.foder@gmail.com>
2019-03-14 21:40:37 +0100

    Dodanie obslugi polecen i pomiaru czasu do maina, utworzenie makefila z testami

--- cw02/zad1/main.c
+++ cw02/zad1/main.c
@@ -1,5 +1,6 @@
 #define _POSIX_C_SOURCE 199309L
 
+#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
@@ -7,6 +8,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <time.h>
+#include <sys/times.h>
 
 
 void printFile(char* filename, unsigned int bytes){
@@ -173,12 +175,179 @@ void generate(char* filename, unsigned int count, unsigned int bytes){
 
 int main(int argc, char* argv[]){
     srand(time(NULL));
-    unsigned int count = 100, bytes = 8;
+    /*unsigned int count = 100, bytes = 8;
     generate("randoms", count, bytes);
     printFile("randoms", bytes);
     sortSys("randoms", bytes);
     printFile("randoms", bytes);
-    copySys("randoms", "randomsCopySys", 4, 500000);
+    copySys("randoms", "randomsCopySys", 31, 500000);*/
+    struct tms usTime;
+    times(&usTime);
+    char resultsFilename[128];
+    char label[128];
+    int savingResults = 0;
+    for(int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "test") == 0) {
+            if (i + 2 >= argc) {
+                printf("%s%s%s",
+                       "Incorrect argument in main, expected two strings (results filename and tests label) after: ",
+                       argv[i], "\n");
+                return -1;
+            }
+            if (sscanf(argv[++i], "%s", resultsFilename) == EOF) {
+                printf("%s%s%s", "Incorrect argument in main, expected string (results filename) after: ", argv[i - 1],
+                       "\n");
+                return -1;
+            }
+            if (sscanf(argv[++i], "%s", label) == EOF) {
+                printf("%s%s%s", "Incorrect argument in main, expected string (tests label) as a second parameter of: ",
+                       argv[i - 2], "\n");
+                return -1;
+            }
+            savingResults = 1;
+        } else if (strcmp(argv[i], "generate") == 0) {
+            if (i + 3 >= argc) {
+                printf("%s%s%s",
+                       "Incorrect argument in main, expected string (file to save results) and two integers (records amount and bytes per record) after: ",
+                       argv[i], "\n");
+                return -1;
+            }
+            char filename[128];
+            int recordsAmount, bytes;
+            if (sscanf(argv[++i], "%s", filename) == EOF) {
+                printf("%s%s%s", "Incorrect argument in main, expected string (filename) after: ", argv[i - 1], "\n");
+                return -1;
+            }
+            if (sscanf(argv[++i], "%d", &recordsAmount) == EOF) {
+                printf("%s%s%s", "Incorrect argument in main, expected integer (records amount) after: ", argv[i - 2], "\n");
+                return -1;
+            }
+            if (sscanf(argv[++i], "%d", &bytes) == EOF) {
+                printf("%s%s%s", "Incorrect argument in main, expected integer (bytes per record) as a third parameter of: ", argv[i - 3], "\n");
+                return -1;
+            }
+            if (recordsAmount <= 0 || bytes <= 0) {
+                printf("Incorrect arguments, both records amount and bytes per record cannot be lower or equal to 0\n");
+                return -1;
+            }
+            generate(filename, recordsAmount, bytes);
+        }
+        else if (strcmp(argv[i], "copy_sys") == 0) {
+            if (i + 4 >= argc) {
+                printf("%s%s%s",
+                       "Incorrect argument in main, expected two strings (source and destination file names) and two integers (bytes in buffer and records to copy) after: ", argv[i], "\n");
+                return -1;
+            }
+            char sourceFilename[128], destinationFilename[128];
+            int bytes, records;
+            if (sscanf(argv[++i], "%s", sourceFilename) == EOF) {
+                printf("%s%s%s", "Incorrect argument in main, expected string (source filename) after: ", argv[i - 1], "\n");
+                return -1;
+            }
+            if (sscanf(argv[++i], "%s", destinationFilename) == EOF) {
+                printf("%s%s%s", "Incorrect argument in main, expected string (destination filename) after: ", argv[i - 2], "\n");
+                return -1;
+            }
+            if (sscanf(argv[++i], "%d", &bytes) == EOF) {
+                printf("%s%s%s", "Incorrect argument in main, expected integer (bytes in buffer) as a third parameter of: ", argv[i - 3], "\n");
+                return -1;
+            }
+            if (sscanf(argv[++i], "%d", &records) == EOF) {
+                printf("%s%s%s", "Incorrect argument in main, expected integer (records to copy) as a fourth parameter of: ", argv[i - 4], "\n");
+                return -1;
+            }
+            if (bytes <= 0 || records <= 0) {
+                printf("Incorrect argument, bytes in buffer and records to copy cannot be lower or equal to 0\n");
+                return -1;
+            }
+            copySys(sourceFilename, destinationFilename, bytes, records);
+        }
+        else if (strcmp(argv[i], "copy_lib") == 0) {
+            if (i + 4 >= argc) {
+                printf("%s%s%s", "Incorrect argument in main, expected two strings (source and destination file names) and two integers (bytes in buffer and records to copy) after: ", argv[i], "\n");
+                return -1;
+            }
+            char sourceFilename[128], destinationFilename[128];
+            int bytes, records;
+            if (sscanf(argv[++i], "%s", sourceFilename) == EOF) {
+                printf("%s%s%s", "Incorrect argument in main, expected string (source filename) after: ", argv[i - 1], "\n");
+                return -1;
+            }
+            if (sscanf(argv[++i], "%s", destinationFilename) == EOF) {
+                printf("%s%s%s", "Incorrect argument in main, expected string (destination filename) after: ", argv[i - 2], "\n");
+                return -1;
+            }
+            if (sscanf(argv[++i], "%d", &bytes) == EOF) {
+                printf("%s%s%s", "Incorrect argument in main, expected integer (bytes in buffer) as a third parameter of: ", argv[i - 3], "\n");
+                return -1;
+            }
+            if (sscanf(argv[++i], "%d", &records) == EOF) {
+                printf("%s%s%s", "Incorrect argument in main, expected integer (records to copy) as a fourth parameter of: ", argv[i - 4], "\n");
+                return -1;
+            }
+            if (bytes <= 0 || records <= 0) {
+                printf("Incorrect argument, bytes in buffer and records to copy cannot be lower or equal to 0\n");
+                return -1;
+            }
+            copyLib(sourceFilename, destinationFilename, bytes, records);
+        }
+        else if (strcmp(argv[i], "sort_sys") == 0) {
+            if (i + 2 >= argc) {
+                printf("%s%s%s", "Incorrect argument in main, string (name of file to sort) and integer (bytes per record) after: ", argv[i], "\n");
+                return -1;
+            }
+            char filename[128];
+            int bytes;
+            if (sscanf(argv[++i], "%s", filename) == EOF) {
+                printf("%s%s%s", "Incorrect argument in main, expected string (name of file to sort) after: ", argv[i - 1], "\n");
+                return -1;
+            }
+            if (sscanf(argv[++i], "%d", &bytes) == EOF) {
+                printf("%s%s%s", "Incorrect argument in main, expected integer (bytes per record) after: ", argv[i - 2], "\n");
+                return -1;
+            }
+            if (bytes <= 0) {
+                printf("Incorrect argument, bytes per record cannot be lower or equal to 0\n");
+                return -1;
+            }
+            sortSys(filename, bytes);
+        }
+        else if (strcmp(argv[i], "sort_lib") == 0) {
+            if (i + 2 >= argc) {
+                printf("%s%s%s", "Incorrect argument in main, string (name of file to sort) and integer (bytes per record) after: ", argv[i], "\n");
+                return -1;
+            }
+            char filename[128];
+            int bytes;
+            if (sscanf(argv[++i], "%s", filename) == EOF) {
+                printf("%s%s%s", "Incorrect argument in main, expected string (name of file to sort) after: ", argv[i - 1], "\n");
+                return -1;
+            }
+            if (sscanf(argv[++i], "%d", &bytes) == EOF) {
+                printf("%s%s%s", "Incorrect argument in main, expected integer (bytes per record) after: ", argv[i - 2], "\n");
+                return -1;
+            }
+            if (bytes <= 0) {
+                printf("Incorrect argument, bytes per record cannot be lower or equal to 0\n");
+                return -1;
+            }
+            sortLib(filename, bytes);
+        }
+    }
+    if(savingResults == 1) {
+        struct tms usTimeEnd;
+        times(&usTimeEnd);
+        usTime.tms_utime = usTimeEnd.tms_utime - usTime.tms_utime;
+        usTime.tms_stime = usTimeEnd.tms_stime - usTime.tms_stime;
+        usTime.tms_cutime = usTimeEnd.tms_cutime - usTime.tms_cutime;
+        usTime.tms_cstime = usTimeEnd.tms_cstime - usTime.tms_cstime;
+        FILE* resultsFile = fopen(resultsFilename, "a");
+        fprintf(resultsFile, "%s:\n", label);
+        int clockTicks = sysconf(_SC_CLK_TCK);
+        fprintf(resultsFile, "%s%f%s", "System time: ", usTime.tms_cstime/(float)clockTicks + usTime.tms_stime/(float)clockTicks, "\n");
+        fprintf(resultsFile, "%s%f%s", "User time: ", usTime.tms_cutime/(float)clockTicks + usTime.tms_utime/(float)clockTicks, "\n\n");
+    }
+
     return 0;
 }
   
